c2d3a7051b98a908eb87dc540e9d85b59f47658a
ecebusracivelek <139459777+ecebusracivelek@users.noreply.github.com>
2024-02-12 19:42:28 +0300

    Create SmallTrucks.java

---
+++ SmallTrucks.java
@@ -0,0 +1,11 @@
+
+package VehicleHiringSystem;
+
+
+ public class SmallTrucks extends Truck {
+        
+    public SmallTrucks(int plateNumber, int numberOfTires,int dailyFee,boolean available,int loadingCapacity){     
+        super(plateNumber,numberOfTires,dailyFee,available,loadingCapacity);
+   
+    }
+}
